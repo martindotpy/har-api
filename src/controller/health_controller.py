03079594eaa9e451e4f6dce9bd6037a2ca220848
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-05-29 03:17:00 -0500

    feat: Hide health check

--- src/controller/health_controller.py
+++ src/controller/health_controller.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
 
-health_router = APIRouter(
-    tags=["health"],
-)
+health_router = APIRouter()
 
 
-@health_router.get("/health")
+@health_router.get("/health", include_in_schema=False)
 def health_check() -> str:
     """Health check endpoint."""
     return "Ok"
